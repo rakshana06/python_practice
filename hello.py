694e87a6505d5e42a31f772c5f0136e2f772a5f8
rakshana06 <rakshanarj06@gmail.com>
2025-07-26 11:53:39 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("Hello,Github!")
+# app.py (instead of hello.py)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, from a web server in Cloud Shell!'
+
+if __name__ == '__main_':
+    app.run(host='0.0.0.0', port=8080)
