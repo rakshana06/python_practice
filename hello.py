179f3ec98cc0c2d8a9f5eafeb1665ce992b43c9b
rakshana06 <rakshanarj06@gmail.com>
2025-08-01 14:19:47 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,9 +3,9 @@ from flask import Flask
 
 hello= Flask(__name__)
 
-@app.route('/')
+@hello.route('/')
 def hello_world():
     return 'Hello, from a web server in Cloud Shell!'
 
 if __name__ == '__main_':
-    app.run(host='0.0.0.0', port=8080)
+    hello.run(host='0.0.0.0', port=8080)
