863fc0a7ef5d1220b9d419bf6b5760597d3004f5
rakshana06 <rakshanarj06@gmail.com>
2025-11-06 03:29:41 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
 from flask import Flask 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/')
 def greet():
     """Return a custom friendly HTTP greeting."""
     return "Hi Everyone ! I am running through cloud shell from rakshana log in"
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     app_host = "http://127.0.0.1:8080"
     print(f"App host link: {app_host}")
     app.run(host="127.0.0.1", port=8080, debug=True)
